a0d574a40f47a5ea02ea41af575e8600706a9e55
vmp6513 <vmp6513@email.com>
2018-10-22 00:02:06 +0800

    上传了第三周的持仓，把预测5天变为预测未来一天

--- Ngt5_lgb.py
+++ Ngt5_lgb.py
@@ -8,28 +8,29 @@ import xlrd
 from datetime import datetime,timedelta
 
 def fund_catagory():#将基金按照ID分开成222个基金
-    fund_product = pd.read_csv(r'./data/fundProduct.csv', index_col='Date')
-    financial_product = pd.read_excel(r'./data/financialProduct.xlsx')
-    fund_num = fund_product['Fund_ID'].groupby(fund_product.Fund_ID).count()
+    fund_product = pd.read_csv(r'./data/week_pre3.csv', index_col='Date')
+    fund_num = fund_product['fund_id'].groupby(fund_product.fund_id).count()
     fund = []
     for i in range(fund_num.shape[0]):
-        each_fund = fund_product[fund_product['Fund_ID'].isin([i + 1])]
+        each_fund = fund_product[fund_product['fund_id'].isin([i + 1])]
         fund.append(each_fund)
     return fund
 
 def main():
     fund = fund_catagory()
     y_pred=[]#222个基金未来5天的预测收盘价
-    for i in range(len(fund)):
-        fund[i]=fund[i][['close','BBI','MA','MACD','KDJ']]#特征选择
-        X=fund[i][['BBI','MA','MACD','KDJ']]
-        Y=fund[i]['close'].shift(-5)
+    for i in range(203):
+        fund[i]=fund[i][['close','NAV_adj','BBI','MA','MACD','KDJ']]#特征选择
+        X=fund[i][['NAV_adj','BBI','MA','MACD','KDJ']]
+        Y=fund[i]['close'].shift(-1)
         X.fillna(method='ffill',inplace=True)
         X=np.array(X)
+        X.astype(np.float32)
         X_scaled = preprocessing.scale(X)
-        X_train = X_scaled[:-5]
-        X_predict = X_scaled[-5:]
-        y_train = np.array(Y.iloc[:-5])
+        X_train = X_scaled[:-1]
+        X_predict = X_scaled[-1]
+        y_train = np.array(Y.iloc[:-1])
+        y_train.astype(np.float32)
 
         print('Start training {} fund'.format(i+1))
         # clf=LinearRegression(n_jobs=-1)
@@ -37,25 +38,25 @@ def main():
         # y_predict = clf.predict(X_predict).tolist()
         gbm = lgb.LGBMRegressor(objective='regression', num_leaves=31, learning_rate=1, n_estimators=25)
         gbm.fit(X_train, y_train, eval_metric='l1')
-        y_predict = gbm.predict(X_predict).tolist()
+        y_predict = gbm.predict([X_predict]).tolist()
         y_pred.append(y_predict)
 
     return y_pred
 
 if __name__=='__main__':
     y_pred=main()
-    df_pred = pd.DataFrame(y_pred, columns=['day1', 'day2', 'day3', 'day4', 'day5'])#预测的未来5天的股价
+    df_pred = pd.DataFrame(y_pred, columns=['day1'])#预测的未来1天的股价
     col_name = df_pred.columns.tolist()
     col_name.insert(0,'day_old')    #每个股票最后一天的股价,放在第一列
     df_pred=df_pred.reindex(columns=col_name)
 
     fund = fund_catagory()
-    for i in range(len(fund)):
+    for i in range(203):
         df_pred.ix[i,'day_old']=fund[i].ix[-1,'close']
-    df_pred['diff']=df_pred['day5']-df_pred['day_old']
+    df_pred['diff']=df_pred['day1']-df_pred['day_old']
     df_pred['stock_return'] = df_pred['diff'] / df_pred['day_old']
     df_pred.sort_values(by='stock_return',ascending=False,inplace=True)
 
-    df_pred.to_csv(r'./data/Ngt5_lgb.csv')
+    df_pred.to_csv(r'./data/Ngt5_lgb_week3.csv')
 
 
--- combine_every_week.py
+++ combine_every_week.py
@@ -2,18 +2,24 @@ import pandas as pd
 from datetime import datetime
 import numpy as np
 #合并第一周和第二周的基金数据
-week1=pd.read_csv('./data/fundProduct.csv')
-week2=pd.read_csv('./data/week2.csv')
-week2.drop('Unnamed: 0',axis=1,inplace=True)
-week2.rename(columns={'fund_id':'Fund_ID'},inplace=True)
+week1_2=pd.read_csv('./data/week1_2.csv')
+week3=pd.read_csv('./data/week3.csv')
+week1_2.drop('Unnamed: 0',axis=1,inplace=True)
+week3.drop('Unnamed: 0',axis=1,inplace=True)
+week1_2.rename(columns={'Fund_ID':'fund_id'},inplace=True)
 
-# print(week1.columns.values)
-# print(week2.columns.values)
-week=pd.concat([week1,week2],axis=0,ignore_index=True)#连接两个df,ignore_index=True表示不保留连接轴上的索引，产生一组新索引range(total_length)
+# print(week1_2.columns.values)
+# print(week3.columns.values)
+
+week1_2['fund_id']=week1_2['fund_id'].astype(int)
+week3['fund_id']=week3['fund_id'].astype(int)
+print(week1_2['fund_id'].dtype)
+print(week3['fund_id'].dtype)
+week=pd.concat([week1_2,week3],axis=0,ignore_index=True)#连接两个df,ignore_index=True表示不保留连接轴上的索引，产生一组新索引range(total_length)
 week['Date']=pd.to_datetime(week['Date'],format='%Y/%m/%d')#把str变为time,才能比较
 week.drop_duplicates(inplace=True)  #删除重复行
-week.sort_values(by=['Fund_ID','Date'],inplace=True)    #先按Fund_ID排序，再按Date排序
+week.sort_values(by=['fund_id','Date'],inplace=True)    #先按Fund_ID排序，再按Date排序
 week.reset_index(drop=True,inplace=True)    #把索引再重新排一下
 
 print(week.head())
-week.to_csv('./data/week1_2.csv')
+week.to_csv('./data/week_pre3.csv')#有点小问题，没去重
